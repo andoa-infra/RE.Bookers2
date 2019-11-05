0cd3dcb92deb89f7e29474eba72be22c452e394a
andoa-infra <ando.shoya.infratop@gmail.com>
2019-11-05 04:53:00 +0000

    First_commit_of_RE.Bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,36 @@
+class BooksController < ApplicationController
+	def new
+		@book = Book.new
+	end
+
+	def create
+		book = Book.new(book_params)
+		book.save
+		redirect_to book_path(book) #(book.id)は(book)のように省略して書ける！
+	end
+
+	def index
+		@books = Book.all
+	end
+
+	def show
+		@book = Book.find(params[:id])
+	end
+
+	def edit
+	    @book = Book.find(params[:id])
+	end
+
+	def update
+		book = Book.find(params[:id])
+		book.update(book_params)
+		redirect_to book_path(book.id)
+	end
+
+
+    private
+    def book_params
+    	params.require(:book).permit(:title, :body, :image)
+    end
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e968152dcfd1f2a2e974ebaca9d06abf2510c8fa2ec8c9beef1d235ff6d2e83e271a8210b68084c07486dba280fa9233a89f0c2cde988aa6cff4700f24a8c545'
